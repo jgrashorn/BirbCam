b0e6bc5e4129c35bba8cc4afe942c03ccb0b941c
BirdPi <j.grashorn@online.de>
2024-10-27 12:08:04 +0100

    QOL features

--- Camera/cameraPython.py
+++ Camera/cameraPython.py
@@ -1,6 +1,7 @@
 import time
 import datetime
 import os
+import logging
 
 import numpy as np
 
@@ -11,131 +12,144 @@ from picamera2.outputs import CircularOutput, FfmpegOutput
 import libcamera
 import birdCamera
 
-config = birdCamera.readConfig()
+logger = logging.getLogger(__name__)
 
-NasFolderName = '' # placeholder if videos should be stored in some other folder (e.g. a NAS)
+def runCamera():
 
-lsize = (320, 240)
-msize = (config["width"], config["height"])
-picam2 = Picamera2()
-
-video_config = picam2.create_video_configuration(main={"size": msize, "format": "YUV420"},
-                                                 lores={"size": lsize, "format": "YUV420"},
-                                                 controls={"ColourGains": (1.0, 1.325)}
-                                                 )
-video_config["transform"] = libcamera.Transform(hflip=0, vflip=0)
-
-# transforms if camera is not oriented right side up
-picam2.configure(video_config)
-encoder = H264Encoder(2000000)
-
-streamOutput = FfmpegOutput(f'-r 30 -f mpegts udp://{config["serverIP"]}:{config["streamPort"]}?pkt_size=1316', audio=False)
-mp4Output = CircularOutput()
-encoder.output = [streamOutput,mp4Output]
-picam2.encoders = encoder
-picam2.start()
-picam2.start_encoder()
-
-w, h = lsize
-
-encoding = False
-
-ltime = 0
-starttime = 0
-fname = ""
-
-failedToSend = []
-
-prev = picam2.capture_buffer("lores")
-prev = prev[:w * h].reshape(h, w).astype(np.int16)
-
-mseSensitivity = config["sensitivity"]
-pixelThreshold = config["numPixelsThreshold"]
-detectionThreshold = config["detectionThreshold"]
-
-bwMode = False
-currBrightness = 0
-skipNFrames = 1
-
-while True:
-
-    cur = picam2.capture_buffer("lores")
-    cur = cur[:w * h].reshape(h, w).astype(np.int16)
-
-    currBrightness = np.square(cur).mean()
-
-    if skipNFrames > 0:
-        skipNFrames -= 1
-
-    else:
-
-        if bwMode and currBrightness > config["clrSwitchingSensitivity"]:
-            print("switching mode")
-            print("clr")
-            picam2.set_controls({"Saturation": 1.0})
-            bwMode = False
-            skipNFrames = config["skippedFramesAfterChange"]
-
-        elif not bwMode and currBrightness < config["bwSwitchingSensitivity"]:
-            print("switching mode")
-            print("b/w")
-            picam2.set_controls({"Saturation": 0.0})
-            bwMode = True
-            skipNFrames = config["skippedFramesAfterChange"]
-
-        # Measure pixels differences between current and
-        # previous frame
-        
-        diff = np.subtract(cur, prev)
-        
-        diffComp = (np.abs(diff) > detectionThreshold) * np.ones(diff.shape)
-        
-        mse = np.square(diff).mean()
+    logging.basicConfig(
+                        filename='birb.log',
+                        format='%(asctime)s %(levelname)s %(module)s %(funcName)-8s %(message)s',
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
     
-        if mse > mseSensitivity or np.sum(diffComp) > pixelThreshold:
-            
-            if not encoding:
-                print("Started.")
-                fname = f'Videos/{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}'
-                mp4Output.fileoutput = fname
-                mp4Output.start()
-                
-                mseSensitivity = config["stopSensitivity"]
-                pixelThreshold = config["stopNumPixelsThreshold"]
-                detectionThreshold = config["stopDetectionThreshold"]
+    config = birdCamera.readConfig()
 
-                starttime = time.time()
-                encoding = True
-            
-            print("New Motion, mse: ", mse, " diffSum: ", np.sum(diffComp))
-            ltime = time.time()
-            
+    NasFolderName = '' # placeholder if videos should be stored in some other folder (e.g. a NAS)
+
+    lsize = (320, 240) # size of internal preview for motion detection (smol bc fast)
+    msize = (config["width"], config["height"]) # size of recording from config.txt
+    picam2 = Picamera2()
+
+    video_config = picam2.create_video_configuration(main={"size": msize, "format": "YUV420"}, # format of recording
+                                                    lores={"size": lsize, "format": "YUV420"}, # format of preview
+                                                    controls={"ColourGains": (0.95, 1.325)} # color correction for IR-cams, (r, b)
+                                                    )
+    # transforms if camera is not oriented right side up
+    video_config["transform"] = libcamera.Transform(hflip=0, vflip=0)
+    
+    picam2.configure(video_config)
+    encoder = H264Encoder(2000000)
+
+    streamOutput = FfmpegOutput(f'-r 30 -f mpegts udp://{config["serverIP"]}:{config["streamPort"]}?pkt_size=1316', audio=False)
+    mp4Output = CircularOutput()
+    encoder.output = [streamOutput,mp4Output]
+    picam2.encoders = encoder
+    picam2.start()
+    picam2.start_encoder()
+
+    w, h = lsize
+
+    encoding = False
+
+    ltime = 0
+    starttime = 0
+    fname = ""
+
+    failedToSend = []
+
+    prev = picam2.capture_buffer("lores")
+    prev = prev[:w * h].reshape(h, w).astype(np.int16)
+
+    mseSensitivity = config["sensitivity"]
+    pixelThreshold = config["numPixelsThreshold"]
+    detectionThreshold = config["detectionThreshold"]
+
+    bwMode = False # greyscale mode on/off
+    currBrightness = 0
+    skipNFrames = 10 # skip the first frames to avoid recording on startup
+
+    while True:
+
+        # capture new preview and reshape
+        cur = picam2.capture_buffer("lores")
+        cur = cur[:w * h].reshape(h, w).astype(np.int16)
+        #calculate current brightness
+        currBrightness = np.square(cur).mean()
+
+        # skip some frames, e.g. if mode was changed
+        if skipNFrames > 0: 
+            skipNFrames -= 1
+
+        # if no frames are skipped, do the thing
         else:
-            if encoding and ((time.time() - ltime > config["captureDelay"]) or (time.time() - starttime > config["maxRecordTime"])):
+            # switch mode in case brightness reached threshold, then skip some frames
+            if bwMode and currBrightness > config["clrSwitchingSensitivity"]:
+                logger.info("switching mode to color")
+                picam2.set_controls({"Saturation": 1.0})
+                bwMode = False
+                skipNFrames = config["skippedFramesAfterChange"]
 
-                mp4Output.stop()
-                encoding = False
-                print("Stopped.")
-                
-                mseSensitivity = config["sensitivity"]
-                pixelThreshold = config["numPixelsThreshold"]
-                detectionThreshold = config["detectionThreshold"]
-                
-                cmd = 'ffmpeg -nostats -loglevel 0 -r 30 -i ' + fname + ' -c copy ' + NasFolderName + fname +'.mp4'
-                os.system(cmd)
-                
-                cmd ='rm ' + fname
-                os.system(cmd)
-                
-                # some code to send video to network-server
+            elif not bwMode and currBrightness < config["bwSwitchingSensitivity"]:
+                logger.info("switching mode to greyscale")
+                picam2.set_controls({"Saturation": 0.0})
+                bwMode = True
+                skipNFrames = config["skippedFramesAfterChange"]
 
-                #if not birdCamera.sendVideo(fname + '.mp4',config["serverIP"],config["filePort"]):
-                #    failedToSend.append(fname)
-                #    print("Failed to send file " + fname + '.mp4')
-                #else:
-                #    print("Sent!")
-                #    cmd ='rm ' + fname + '.mp4'
-                #    os.system(cmd)
-                #    fname = ""
+            # measure pixels differences (mse) between current and previous frame
+            diff = np.subtract(cur, prev)
+            mse = np.square(diff).mean()
+
+            # measure individual pixel difference to detect local changes
+            diffComp = (np.abs(diff) > detectionThreshold) * np.ones(diff.shape)
+        
+            if mse > mseSensitivity or np.sum(diffComp) > pixelThreshold:
+                
+                if not encoding:
+                    logger.info("Started recording.")
+                    logger.info( f"New Motion, mse: {mse:5.2f}, diffSum: {np.sum(diffComp):.0f}" )
+                    fname = f'Videos/{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}'
+                    mp4Output.fileoutput = fname
+                    mp4Output.start()
                     
-    prev = cur
+                    # change thresholds for stopping
+                    mseSensitivity = config["stopSensitivity"]
+                    pixelThreshold = config["stopNumPixelsThreshold"]
+                    detectionThreshold = config["stopDetectionThreshold"]
+
+                    starttime = time.time()
+                    encoding = True
+                    
+                ltime = time.time()
+                
+            else:
+                if encoding and ((time.time() - ltime > config["captureDelay"]) or (time.time() - starttime > config["maxRecordTime"])):
+
+                    mp4Output.stop()
+                    encoding = False
+                    logger.info("Stopped.")
+                    
+                    # change thresholds to starting values
+                    mseSensitivity = config["sensitivity"]
+                    pixelThreshold = config["numPixelsThreshold"]
+                    detectionThreshold = config["detectionThreshold"]
+                    
+                    cmd = 'ffmpeg -nostats -loglevel 0 -r 30 -i ' + fname + ' -c copy ' + NasFolderName + fname +'.mp4'
+                    os.system(cmd)
+                    
+                    cmd ='rm ' + fname
+                    os.system(cmd)
+                    
+                    # some code to send video to network-server
+                    #if not birdCamera.sendVideo(fname + '.mp4',config["serverIP"],config["filePort"]):
+                    #    failedToSend.append(fname)
+                    #    print("Failed to send file " + fname + '.mp4')
+                    #else:
+                    #    print("Sent!")
+                    #    cmd ='rm ' + fname + '.mp4'
+                    #    os.system(cmd)
+                    #    fname = ""
+   
+        prev = cur # overwrite previous frame with current one
+
+if __name__ == "__main__":
+    runCamera()
